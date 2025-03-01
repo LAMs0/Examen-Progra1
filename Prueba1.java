1c6918f00b82716ee318e05985c3cb65f9d21ea1
Luis López <dg.luislo8@gmail.com>
2025-02-28 22:57:21 -0800

    Este fue un pequeño cambio

--- Prueba1.java
+++ Prueba1.java
@@ -1,7 +1,7 @@
 public class Prueba1 {
     public static void main(String[] args){
         System.out.println("Mi nombre es Luis Lopez");
-        System.out.println("Estoy aprendiendo Java!");
+        System.out.println("Estoy aprendiendo JAVA2!");
     }
     
   
